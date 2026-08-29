b8f8895a2d2c770e8b6334023f7ded76fce8f2c1
boddusaiprakash123 <95060708+boddusaiprakash123@users.noreply.github.com>
2026-08-29 13:42:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple root route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application.# No.1 Yaari#" });
+  res.json({ message: "Welcome to bezkoder application.# Amma-Nanna" });
 });
 
 // GET /users - fetch all users from database
